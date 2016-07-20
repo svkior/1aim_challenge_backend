7e79c8b6447e6d2a2c15faa840cd0a9d6bdbb9e1
Sergey V. Kior <svkior@gmail.com>
2016-07-20 17:44:01 +0300

    Static fileserver

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "flag"
+)
+
+func main(){
+  httpHost := flag.String("http", ":8090", "Description")
+  flag.Parse()
+  log.Printf("Starting API server on %s", *httpHost)
+  http.Handle("/", http.FileServer(http.Dir("./build")))
+  log.Fatal(http.ListenAndServe(*httpHost, nil))
+}
